c9f1c35ee2bb0ee55f8747b805474ca5407d2954
BTV123 <102411013+BTV123@users.noreply.github.com>
2022-04-11 16:34:31 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,3 +3,19 @@ __winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
 __human_name__ = 'strings'
 
 # Add your code after this line
+
+print ('test')
+
+scorer1 =  'Ruud Gullit'
+scorer2 = 'Marco van Basten'
+goal_0 = 32
+goal_1 = 54
+scorers = scorer1 + " " + str(goal_0) +","+ scorer2 +" " + str(goal_1)
+report = f'{scorer1} scored in the {goal_0}nd minute\n{scorer2} scored in the {goal_1}the minute'
+player = 'Edwin van der Sar'
+first_name = player[:player.find(" ")]
+last_name = player[player.find(" "):]
+last_name_len = len(last_name)
+name_short = player[player.find(" ") :]
+chand = 'Goallll Goalll' + last_name
+print(chand)
